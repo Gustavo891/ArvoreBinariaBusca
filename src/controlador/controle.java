7979a8a206ef02e788d9e41fbb1fb61d0af38d39
Luiz Gustavo <86622386+Gustavo891@users.noreply.github.com>
2023-11-13 20:03:21 -0300

    Update controle.java

--- src/controlador/controle.java
+++ src/controlador/controle.java
@@ -1,16 +1,14 @@
-
 package controlador;
 
 import visao.visualizar;
 
 public class controle {
-    public controle() {
-    }
+    public static void main(String[] args) {
+        visualizar visao = new visualizar();
 
-    public static void main(String[] var0) {
-        visualizar var1 = new visualizar();
         System.out.println("Árvore Binária de Busca - Teste de Completude");
-        var1.carregarArvore();
-        var1.executarComandos();
+        visao.carregarArvore();
+        visao.executarComandos();
+
     }
 }
